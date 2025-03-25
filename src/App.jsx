133d3d76839b55078978756b4522087ab6a65e79
athulya anil <athulyaanil@Athulyas-MacBook-Pro.local>
2025-03-25 13:53:23 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        ✅ Tailwind is Working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
