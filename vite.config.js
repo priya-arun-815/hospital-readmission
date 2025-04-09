7cc0b02a961c978997963933f2b196cbe9f41137
Indirapriyadarshini Arunachalam <priya.a2105@gmail.com>
2025-04-09 14:50:34 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hospital-readmission/' // 👈 GitHub repo name
+  base: '/hospital-readmission/' // GitHub repo name
 })
 
